6552d2f6b5972cd6aa558abea60b752db7ca6079
Zekiah-A <zekiahamoako@outlook.com>
2021-06-18 22:07:55 +0100

    Editor menu improvements, open/close and tween

--- TitleUiManager.cs
+++ TitleUiManager.cs
@@ -7,10 +7,12 @@ public class TitleUiManager : Node
 	private Panel _settingsPanel;
 	private Panel _gamemodePanel;
 	private Panel _matchsettingsPanel;
+	private Panel _editorPanel;
 
 	private Tween _settingsTween;
 	private Tween _gamemodeTween;
 	private Tween _matchsettingsTween;
+	private Tween _editorTween;
 
 	public override void _Ready()
 	{
@@ -18,14 +20,17 @@ public class TitleUiManager : Node
 		_settingsPanel = (Panel) GetParent().GetNode("Settings Panel");
 		_gamemodePanel = (Panel) GetParent().GetNode("Gamemode Panel");
 		_matchsettingsPanel = (Panel) GetParent().GetNode("Matchsettings Panel");
+		_editorPanel = (Panel) GetParent().GetNode("Editor Panel");
 		
 		_settingsTween = (Tween) GetParent().GetNode("Settings Panel").GetNode("Panel Tween");
 		_gamemodeTween = (Tween) GetParent().GetNode("Gamemode Panel").GetNode("Panel Tween");
 		_matchsettingsTween = (Tween) GetParent().GetNode("Matchsettings Panel").GetNode("Panel Tween");
+		_editorTween = (Tween) GetParent().GetNode("Editor Panel").GetNode("Panel Tween");
 		
 		_settingsPanel.Visible = false;
 		_gamemodePanel.Visible = false;
 		_matchsettingsPanel.Visible = false;
+		_editorPanel.Visible = false;
 	}
 //  public override void _Process(float delta) {}
 	
@@ -95,11 +100,31 @@ public class TitleUiManager : Node
 			case 3:
 				_matchsettingsPanel.Visible = false;
 				break;
+			case 4:
+				_editorPanel.Visible = false;
+				break;
 		}
 	}
+	
 	private void _on_GraphicsQuality_pressed(int _index)
 	{
 		GD.Print($"Graphics quality level set to {_index}");
+	}
+	
+	private void _on_Editor_pressed()
+	{
+		_editorPanel.Visible = true;
+
+		_editorTween.InterpolateProperty (
+			_editorPanel, //Object
+			"rect_position", //Property being tweened
+			new Vector2(0, 600), //from
+			new Vector2(0,0), //to
+			1, //speed
+			Tween.TransitionType.Cubic,
+			Tween.EaseType.Out
+		);
+		_editorTween.Start();
 	}
 	#endregion
 }
