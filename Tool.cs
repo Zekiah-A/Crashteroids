175223ac5b4e88d1b33f471c156bcb80c8df42f0
Zekiah-A <zekiahamoako@outlook.com>
2021-10-25 18:54:03 +0100

    Shop & Tools update

--- Tool.cs
+++ Tool.cs
@@ -1,11 +1,11 @@
 using Godot;
 using System;
 
-public class Tool : Node
+public class Tool : Panel
 {
+/*
 	[Export] public int Id = 0;
 	[Export] public int Price = 0;
-
 	[Export]
 	public bool Bought
 	{
@@ -16,31 +16,27 @@ public class Tool : Node
 			BoughtChanged();
 		}
 	}
-
 	private bool bought;
+*/
 	private StyleBoxFlat defaultStyle;
 	private StyleBoxFlat boughtStyle;
-	/*private StyleBoxFlat equippedStyle;*/
 	private Label description;
 
 	public override void _Ready()
 	{
 		defaultStyle = ResourceLoader.Load("res://styles/editoritem_available.tres") as StyleBoxFlat;
 		boughtStyle = ResourceLoader.Load("res://styles/editoritem_bought.tres") as StyleBoxFlat;
-		/*equippedStyle = ResourceLoader.Load("res://styles/editoritem_equipped.tres") as StyleBoxFlat;*/
 
 		description = GetNode<Label>("Description");
-		//TODO: Utils.AddCommasToNumbers function
     }
 
-	public void BoughtChanged()
+	public void Buy()
 	{
-		if (bought)
-		{
+		//if (bought)
+		//{
 			description.Text = "Bought";
 			description.AddStyleboxOverride("normal", boughtStyle);
-			//play bought animation (maybe make a fun gloss shader for these as well when item is available)
 			GetNode<AnimationPlayer>("AnimationPlayer").Play("bought_anim");
-		}
+		//}
 	}
 }
