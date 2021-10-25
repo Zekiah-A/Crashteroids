ff48b701d3babab0881a78e94bd911a2d21d2b84
Zekiah-A <zekiahamoako@outlook.com>
2021-10-25 18:54:03 +0100

    Shop & Tools update

--- ShopUiManager.cs
+++ ShopUiManager.cs
@@ -5,21 +5,28 @@ using System.Collections.Generic;
 public class ShopUiManager : Panel
 {
 	//TODO: Make public in config?
-	private List<Tools> upgrades;
+	private Dictionary<ToolTypes, Tool> upgrades;
 
 	public override void _Ready()
 	{
-		
+		upgrades = new Dictionary<ToolTypes, Tool>()
+		{
+			{ ToolTypes.Pen, GetNode("Main Panel").GetNode("Items Panel").GetNode("1") as Tool },
+			{ ToolTypes.Potractor, GetNode("Main Panel").GetNode("Items Panel").GetNode("2") as Tool },
+			{ ToolTypes.Ruler, GetNode("Main Panel").GetNode("Items Panel").GetNode("3") as Tool },
+			{ ToolTypes.Laser, GetNode("Main Panel").GetNode("Items Panel").GetNode("4") as Tool }
+		};
 	}
 
-	private void OnItemClick(int selected)
+	private void UpgradePressed(int selected)
 	{
-
+		(upgrades[(ToolTypes)selected] as Tool).Buy();
+		//TODO: Save bought to config & Deduct money.
 	}
 }
 
-//TODO: Move to it's own file!
-enum Tools
+//TODO: Move to it's own file?
+enum ToolTypes
 {
 	Pen,
 	Potractor,
